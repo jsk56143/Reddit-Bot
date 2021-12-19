713708c2da18346fad08eb2743b88fced091e71f
Josh Kim <jsk56143@uga.edu>
2021-12-19 02:06:28 -0500

    Added variety and music comment

--- src/constants.py
+++ src/constants.py
@@ -16,3 +16,30 @@ Unfortunately, this post has been removed because I have determined that this mi
 
 
 _I am a bot, and this action was performed automatically. Please contact the [moderators of this subreddit](https://www.reddit.com/message/compose?to=/r/test484) if you have any questions or concerns._"""
+
+
+varietyComment = """Thank you for submitting to r/khiphop!
+
+Unfortunately, this post has been removed because I have determined that you did not include the language in brackets. Please remember this goes at the end of the post title. 
+
+* Please use [RAW] if Korean is primarily spoken. 
+
+* Please use [ENG] if English is primarily spoken.
+
+* Please use [ENG SUB] if English subtitles are provided. 
+
+---
+
+
+_I am a bot, and this action was performed automatically. Please contact the [moderators of this subreddit](https://www.reddit.com/message/compose?to=/r/test484) if you have any questions or concerns._"""
+
+musicComment = """Thank you for submitting to r/khiphop!
+
+Unfortunately, this post has been removed because I have determined that you did not follow Rule 3: Title Guidelines. Please make sure that you follow this format:
+
+>Artist - Track (feat. if applicable)
+
+---
+
+
+_I am a bot, and this action was performed automatically. Please contact the [moderators of this subreddit](https://www.reddit.com/message/compose?to=/r/test484) if you have any questions or concerns._"""
