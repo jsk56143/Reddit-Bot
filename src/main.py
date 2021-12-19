7f847dc7e1fbe9789b0405b7e189da81c32c11aa
Josh Kim <jsk56143@uga.edu>
2021-12-19 02:06:50 -0500

    Made functions and added new functions

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@
 
 import praw
 import os
-from constants import teaserComment, discussionComment
+from constants import teaserComment, discussionComment, varietyComment, musicComment
 
 reddit = praw.Reddit(
     client_id = os.environ['reddit_client_id'],
@@ -16,19 +16,48 @@ reddit = praw.Reddit(
 )
 
 # Input subreddit name
-subreddit = reddit.subreddit("khiphop")
+subreddit = reddit.subreddit("test484")
 
-# Check stream for rule-breaking posts
-for submission in subreddit.stream.submissions(skip_existing=True):
-    if submission.link_flair_text == "Teaser":
-        comment = submission.reply(teaserComment)
-        comment.mod.distinguish(how='yes', sticky=True)
-        submission.mod.approve()
-    elif submission.is_self and submission.link_flair_text == "Discussion" and len(submission.selftext) < 450:
+# TEASER
+def replyToTeaserPost(submission):
+    comment = submission.reply(teaserComment)
+    comment.mod.distinguish(how='yes', sticky=True)
+    submission.mod.approve()
+
+def addToArtCollection(submission):
+    if "i.redd.it" in submission.url:
+        print('Hello Worlf!')
+
+#DISCUSSION
+def checkPostBodyLength(submission):
+    if submission.is_self and len(submission.selftext) < 450:
         comment = submission.reply(discussionComment)
         comment.mod.distinguish(how='yes', sticky=True) 
         submission.mod.remove(mod_note="Body too short")
 
+#VARIETY
+def checkLangAtEnd(submission):
+    title = submission.title
+    if "[ENG]" not in title or "[ENG SUB]" not in title or "[RAW]" not in title:
+        comment = submission.reply(varietyComment)
+        comment.mod.distinguish(how='yes', sticky=True) 
+        submission.mod.remove()
 
+#MUSIC
+def checkDash(submission):
+    if " - " not in submission.title:
+        comment = submission.reply(musicComment)
+        comment.mod.distinguish(how='yes', sticky=True) 
+        submission.mod.remove()
 
-
+# Check stream for rule-breaking posts
+for submission in subreddit.stream.submissions(skip_existing=True):
+    if submission.link_flair_text == "Teaser":
+        replyToTeaserPost(submission)
+        addToArtCollection(submission)
+    elif submission.link_flair_text == "Discussion":
+        checkPostBodyLength(submission)
+    elif submission.link_flair_text == "Variety":
+        checkLangAtEnd(submission)
+    elif submission.link_flair_text == "Music Video" or submission.link_flair_text == "Audio" or submission.link_flair_text == "Live" or submission.link_flair_text == "Album":
+        checkDash(submission)
